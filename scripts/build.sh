78617c295e074eb08836323048d8c42661f4d734
Rhahi <git@rhahi.com>
2023-10-22 01:30:59 +0200

    dev: add automatic version revision for HTML

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,3 +22,10 @@ for FILE in "./data"/*; do
         toml2json --pretty "$FILE" > "./app/data/$NAME.json"
     fi
 done
+
+# apply version in HTML
+PACKAGE_JSON_PATH="./package.json"
+HTML_PATH="./app/html/sidebar.html"
+VERSION=$(npm list --json | jq -r ".version")
+sed -i "s/\(footer id=\"version\"\)[^<]*</\1>$VERSION</" "$HTML_PATH"
+echo "Version updated to $VERSION"
