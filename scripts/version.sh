9f58840433461f68d377303eb6fd31f2a0ee8587
Rhahi <git@rhahi.com>
2023-10-22 01:41:26 +0200

    dev: make version.sh also commit

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,4 +4,5 @@ HTML_PATH="./app/html/sidebar.html"
 VERSION=$(npm list --json | jq -r ".version")
 sed -i "s/\(footer id=\"version\"\)[^<]*</\1>$VERSION</" "$HTML_PATH"
 git add "./app/html/sidebar.html"
+git commit --amend --no-edit
 echo "Version updated to $VERSION"
