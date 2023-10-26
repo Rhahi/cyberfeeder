419dbafcfa766d81710e8da7a63738bd23378447
Rhahi <git@rhahi.com>
2023-10-27 00:22:54 +0200

    fix: version script typo

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+set -e
 mkdir -p "./tmp"
 PACKAGE_JSON_PATH="./package.json"
-JSON_PATH="./app/mainfest.json"
+JSON_PATH="./app/manifest.json"
 VERSION=$(npm list --json | jq -r ".version")
 jq --arg v "$VERSION" '.version = $v' "$JSON_PATH" > "./tmp/manifest.json" && mv "./tmp/manifest.json" "$JSON_PATH"
 
