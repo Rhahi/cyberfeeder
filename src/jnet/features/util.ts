7d733702945aeb313922c35f86070ec634693a60
Rhahi <git@rhahi.com>
2023-11-05 14:03:10 +0100

    feat: add panel parser to utility

--- src/jnet/features/util.ts
+++ src/jnet/features/util.ts
@@ -1,4 +1,12 @@
-export type RunTarget = 'run-rnd' | 'run-hq' | 'run-archives' | 'run-remote' | 'run-unknown' | 'not-in-a-run';
+export type Location = 'rnd' | 'hq' | 'archives' | 'remote' | 'unknown' | 'stack' | 'heap' | 'grip' | 'no-target';
+
+export interface PanelInfo {
+  context?: number;
+  card?: string;
+  text: string;
+  buttons: string[];
+  location: Location;
+}
 
 export function getChat() {
   return document.querySelector('.panel > .log > .messages');
@@ -19,3 +27,72 @@ export function getArrow() {
 export function getChatInputbox() {
   return document.getElementById('log-input');
 }
+
+export function toLocation(text?: string | null): Location {
+  if (text === 'R&D') {
+    return 'rnd';
+  }
+  if (text === 'Archives') {
+    return 'archives';
+  }
+  if (text === 'HQ') {
+    return 'hq';
+  }
+  if (text === 'Server') {
+    return 'remote';
+  }
+  if (text === 'Stack' || text === 'stack') {
+    return 'stack';
+  }
+  if (text === 'Heap') {
+    return 'heap';
+  }
+  return 'unknown';
+}
+
+export function parseCommandPanel(element?: Element, prefix = '> .panel') {
+  let panel: Element | null;
+  if (!element) {
+    panel = getCommandPanel();
+  } else {
+    panel = element;
+  }
+  if (!panel) {
+    return;
+  }
+  const info: PanelInfo = {
+    card: getPanelCardName(panel, prefix),
+    text: getPanelText(panel, prefix),
+    buttons: getPanelButtons(panel, prefix),
+    location: 'unknown',
+  };
+  return info;
+}
+
+function getPanelCardName(panel: Element, prefix = '> .panel') {
+  const card = panel.querySelector(`:scope ${prefix} > div:first-child > span.fake-link`);
+  if (card?.textContent) {
+    return card.textContent;
+  }
+  return;
+}
+
+function getPanelText(panel: Element, prefix = '> .panel') {
+  const text = panel.querySelector(`:scope ${prefix} > h4`);
+  if (text?.textContent) {
+    return text.textContent;
+  }
+  return '';
+}
+
+function getPanelButtons(panel: Element, prefix = '> .panel') {
+  const buttons: string[] = [];
+  const elements = panel.querySelectorAll(`:scope ${prefix} > button`);
+  elements.forEach(button => {
+    if (button.textContent) {
+      buttons.push(button.textContent);
+    }
+  });
+  return buttons;
+}
+
