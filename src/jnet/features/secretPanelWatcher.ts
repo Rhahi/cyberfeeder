5117a3bc1238c95217a7d0c54d598f2f36df7064
Rhahi <git@rhahi.com>
2023-11-08 02:46:17 +0100

    fix: add bottom cards into secret pattern

--- src/jnet/features/secretPanelWatcher.ts
+++ src/jnet/features/secretPanelWatcher.ts
@@ -11,7 +11,7 @@ const maybeSecretPatterns: MatchType[] = [
   // these text will be excluded from being cached
   /^You accessed (?<card>.*)\.(?:$| Pay| Trash| Shuffle| Add| Spend)/,
   /^Add (?<card>.*) to bottom of (?:the )?(?<location>stack)\?$/,
-  /top (?<number>\d )?cards? of (?:the )?(?<location>stack|R&D) (?:is|are|will be) (?<card>.*)$/,
+  /(?:top|bottom) (?<number>\d )?cards? of (?:the )?(?<location>stack|R&D) (?:is|are|will be) (?<card>.*)$/,
 ];
 export const lastClicks: CommandButton[] = [];
 export let lastSecret: util.PanelInfo = {age: -1, text: '', buttons: [], location: 'unknown'};
