09464c9992e9ff82377ded0d19a7248ded100de7
Rhahi <git@rhahi.com>
2023-11-12 19:04:24 +0100

    feat: add watcher for panel creation (happens during replay)

--- src/jnet/watchers/navigation.ts
+++ src/jnet/watchers/navigation.ts
@@ -12,13 +12,21 @@
  * The only interesting item is 'gameview' where cyberfeeder features work.
  */
 export interface Navigation {
+  type: 'change-menu' | 'change-panel';
   mode: string;
   root: Element;
 }
+export const changeMenu = 'change-menu';
+export const changePanel = 'change-panel';
 
-export const eventName = 'watcher-site';
+/** Enable menu navigation watchers. These will start once at startup and will never stop. */
+export function enable() {
+  enableNavigationWatcher();
+  enablePanelCreationWatcher();
+}
 
-export function watch() {
+/** Watch and report main menu navigation event */
+function enableNavigationWatcher() {
   const main = document.querySelector('#main-content #main > .item');
   const scope = 'cyberfeeder';
   if (!main) {
@@ -32,8 +40,8 @@ export function watch() {
     if (firstChild && firstChild.nodeType === Node.ELEMENT_NODE) {
       if (firstChild.className) {
         page = firstChild.className;
-        const data: Navigation = {mode: page, root: firstChild};
-        const event = new CustomEvent<Navigation>(eventName, {detail: data});
+        const data: Navigation = {type: changeMenu, mode: page, root: firstChild};
+        const event = new CustomEvent<Navigation>(changeMenu, {detail: data});
         document.dispatchEvent(event);
       }
     }
@@ -48,4 +56,53 @@ export function watch() {
   });
   siteObserver.observe(main, {childList: true});
   toggleObserver.observe(main, {attributes: true});
+}
+
+/** Watch and report command panel change event */
+function enablePanelCreationWatcher() {
+  const PanelCreationObserver = new MutationObserver(panelCreationHandler);
+  document.addEventListener(changeMenu, (e: Event) => {
+    const src = e as CustomEvent<Navigation>;
+    if (src.detail.type !== changeMenu) {
+      return;
+    }
+    if (src.detail.mode === 'gameview') {
+      // user has navigated in, start a new watch.
+      PanelCreationObserver.disconnect();
+      const pane = document.querySelector('.right-inner-leftpane');
+      if (pane) {
+        PanelCreationObserver.observe(pane, {childList: true});
+      } else {
+        console.warn('[Cyberfeeder] expected to find right-inner-leftpane, found none');
+      }
+    } else {
+      // user has navigated away, no more control elements.
+      PanelCreationObserver.disconnect();
+    }
+  });
+}
+
+/** Check if .right-inner-leftpane got a new .button-pane element. If it did, report it. */
+function panelCreationHandler(mutations: MutationRecord[]) {
+  for (const m of mutations) {
+    let done = false;
+    m.addedNodes.forEach(node => {
+      if (done) {
+        return;
+      }
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const element = node as Element;
+        if (element.className === 'button-pane') {
+          const data: Navigation = {
+            type: changePanel,
+            mode: 'gameview',
+            root: element,
+          };
+          const event = new CustomEvent(changePanel, {detail: data});
+          document.dispatchEvent(event);
+          done = true;
+        }
+      }
+    });
+  }
 }
