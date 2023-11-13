26b3451fcda7ffbd99271315d5ab650d8d1f0f86
Rhahi <git@rhahi.com>
2023-11-14 00:55:03 +0100

    feat: add init for archive watcher

--- src/jnet/watchers/archive.ts
+++ src/jnet/watchers/archive.ts
@@ -7,6 +7,7 @@ import * as base from './base';
 
 export const eventName = 'change-archive';
 const viewChangeObserver = new MutationObserver(viewChangeHandler);
+const selector = '.discard-container .panel.popup';
 
 export interface Archive {
   type: 'change-archive';
@@ -23,6 +24,16 @@ const archiveWatcher = (event: Event) => {
     selector: '.gameview .centralpane',
     observeOptions: {childList: true},
   });
+  base.conditionalExecuter({
+    event,
+    type: base.eventName,
+    targetMode: 'gameview',
+    callback: enable => {
+      if (enable) {
+        init();
+      }
+    },
+  });
 };
 
 function viewChangeHandler(mutations: MutationRecord[]) {
@@ -33,7 +44,7 @@ function viewChangeHandler(mutations: MutationRecord[]) {
     m.addedNodes.forEach(node => {
       if (node.nodeType === Node.ELEMENT_NODE) {
         const element = node as Element;
-        const archive = element.querySelector(':scope .discard-container .panel.popup');
+        const archive = element.querySelector(selector);
         if (!archive) {
           return;
         }
@@ -66,4 +77,20 @@ export function watch() {
 
 export function stop() {
   document.removeEventListener(base.eventName, archiveWatcher);
+}
+
+/** A one time event that will prompt new archives */
+function init() {
+  const me = document.querySelector('.me ' + selector);
+  const opponent = document.querySelector('.opponent ' + selector);
+  if (me) {
+    const data: Archive = {type: 'change-archive', side: 'me', element: me};
+    const event = new CustomEvent<Archive>(eventName, {detail: data});
+    document.dispatchEvent(event);
+  }
+  if (opponent) {
+    const data: Archive = {type: 'change-archive', side: 'opponent', element: opponent};
+    const event = new CustomEvent<Archive>(eventName, {detail: data});
+    document.dispatchEvent(event);
+  }
 }
