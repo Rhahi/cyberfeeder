a90251e1700761f4f30f10febc34a08bdbff2a83
Rhahi <git@rhahi.com>
2023-11-19 17:42:36 +0100

    fix: add index.ts to unify features interface

---
+++ src/jnet/features/index.ts
@@ -0,0 +1,4 @@
+export * as handsizeReminder from './handsizeReminder';
+export * as newMessageIndicator from './newMessageIndicator';
+export * as sortArchive from './sortArchive';
+export * as annotateChat from './annotateChat';
