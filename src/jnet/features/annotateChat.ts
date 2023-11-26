b569dc0b0c2fdcd54ac91b3e9a02aa67284ef67d
Rhahi <git@rhahi.com>
2023-11-26 16:52:09 +0100

    refactor(annotate): make annotateChat more robust
    this is achieved by returning an object that will be used to annotate,
    instead of puttint the icons immediately

--- src/jnet/features/annotateChat.ts
+++ src/jnet/features/annotateChat.ts
@@ -2,11 +2,27 @@ import {chat} from '../watchers';
 import {isFullyDown} from './newMessageIndicator';
 import * as util from './util';
 
+type MatchType = string | RegExp;
+
 const turnRegex = /turn (\d+)/;
-const accessRegex = /accesses .*(?:from|in) (?:the root of )?(?<location>HQ|R&D|Archives|Server)/;
-const exposeRegex = /exposes .*(?<location>HQ|R&D|Archives|Server)/;
-const revealRegex = /^(?!.*install it).*\b(?:uses? .* to reveal|reveals|then reveals?).*(?<location>HQ|R&D|Archives|Server|stack)\b/;
-const addRegex = /add .* to (?<location>HQ|R&D|Archives|grip|stack)/;
+const accessRegex = [/accesses .*(?:from|in) (?:the root of )?(?<location>HQ|R&D|Archives|Server)/];
+const exposeRegex = [/exposes .*(?<location>HQ|R&D|Archives|Server)/];
+const revealRegex = [
+  //
+  /^(?!.*install it).*\b(?:uses? .* to reveal|reveals|then reveals?).*(?<location>HQ|R&D|Archives|Server|stack)\b/,
+  /reveals? that they drew/,
+];
+const addRegex = [/add .* to (?<location>HQ|R&D|Archives|grip|stack)/];
+const bottomRegex = [/adds? .* to (?:the )?bottom of (?:the )?(?<location>stack|R&D)/];
+const discardRegex = [/discards? .* from (?:their )(?<source>Grip|grip|HQ)/];
+
+interface Annotation {
+  done?: boolean;
+  action?: Element;
+  source?: Element;
+  location?: Element;
+  secret?: Element;
+}
 
 const annotate = (e: Event) => {
   const event = e as CustomEvent<chat.ChatMessage>;
@@ -17,11 +33,20 @@ const annotate = (e: Event) => {
     // ignore user messages
     return;
   }
-  if (annotateTurn(event.detail)) return;
-  annotateGeneric('access', accessRegex, event.detail);
-  annotateGeneric('expose', exposeRegex, event.detail);
-  annotateGeneric('reveal', revealRegex, event.detail);
-  annotateGeneric('add', addRegex, event.detail);
+
+  const shouldScroll = event.detail.element.parentElement ? isFullyDown(event.detail.element.parentElement) : false;
+  const annotation: Annotation = {done: false};
+  annotateTurn(annotation, event.detail);
+  annotateGeneric(annotation, 'cf-access', accessRegex, event.detail);
+  annotateGeneric(annotation, 'cf-expose', exposeRegex, event.detail);
+  annotateGeneric(annotation, 'cf-reveal', revealRegex, event.detail);
+  annotateGeneric(annotation, 'cf-bottom', bottomRegex, event.detail);
+  annotateGeneric(annotation, 'cf-add', addRegex, event.detail);
+  annotateGeneric(annotation, 'cf-discard', discardRegex, event.detail);
+  const didAddIcons = addIcons(event.detail.element, annotation);
+  if (didAddIcons && shouldScroll && event.detail.element.parentElement) {
+    event.detail.element.parentElement.scrollTop = event.detail.element.parentElement.scrollHeight;
+  }
 };
 
 export function enable() {
@@ -32,80 +57,78 @@ export function disable() {
   document.removeEventListener(chat.eventName, annotate);
 }
 
-function setFontAwesomeIcon(element: Element, params: {action?: string; location?: util.Location; source?: util.Location}) {
-  const container = document.createElement('div');
+export function createIcon(text: string) {
+  const icon = document.createElement('i');
+  if (text === 'hq') icon.classList.add('fa-sharp', 'fa-solid', 'fa-building', 'icon-hq');
+  else if (text === 'rnd') icon.classList.add('fa-solid', 'fa-flask', 'icon-rnd');
+  else if (text === 'archives') icon.classList.add('fa-solid', 'fa-server', 'icon-archives');
+  else if (text === 'remote') icon.classList.add('fa-sharp', 'fa-solid', 'fa-network-wired', 'icon-remote');
+  else if (text === 'heap') icon.classList.add('fa-solid', 'fa-hard-drive', 'icon-stack');
+  else if (text === 'stack') icon.classList.add('fa-solid', 'fa-layer-group', 'icon-stack');
+  else if (text === 'grip') icon.classList.add('fa-solid', 'fa-hand', 'icon-grip');
+  else if (text === 'right-arrow') icon.classList.add('fa-solid', 'fa-arrow-right', 'icon-right');
+  else if (text === 'secret') icon.classList.add('fa-solid', 'fa-circle-question', 'icon-secret');
+  else if (text === 'cf-turn') icon.classList.add('fa-solid', 'fa-hourglass-half', 'icon-turn');
+  else if (text === 'cf-access') icon.classList.add('fa-solid', 'fa-bolt', 'icon-access');
+  else if (text === 'cf-add') icon.classList.add('fa-solid', 'fa-circle-plus', 'icon-add');
+  else if (text === 'cf-reveal') icon.classList.add('fa-regular', 'fa-eye', 'icon-reveal');
+  else if (text === 'cf-expose') icon.classList.add('fa-solid', 'fa-eye', 'icon-expose');
+  else if (text === 'cf-bottom') icon.classList.add('fa-solid', 'fa-download', 'icon-bottom');
+  else if (text === 'cf-discard') icon.classList.add('fa-solid', 'fa-dumpster', 'icon-discard');
+  else icon.classList.add('fa-solid', 'fa-question', 'icon-unknown');
+  return icon;
+}
+
+export function addIcons(chatDiv: Element, annotation: Annotation) {
+  if (!annotation.action && !annotation.source && !annotation.location) return false;
+
+  let container = chatDiv.querySelector(':scope .cyberfeeder-icon');
+  if (!container) {
+    container = document.createElement('div');
+    chatDiv.prepend(container);
+  }
   container.classList.add('cyberfeeder-icon');
   container.setAttribute('style', 'display: none;');
-  if (params.action) addFontAwesomeIcon(container, params.action);
-  if (params.source && params.source !== 'unknown') {
-    addFontAwesomeIcon(container, params.source);
-    if (params.location && params.location !== 'unknown') addFontAwesomeIcon(container, 'right-arrow');
+  if (annotation.action) container.appendChild(annotation.action);
+  if (annotation.source) {
+    container.appendChild(annotation.source);
+    if (annotation.location) container.appendChild(createIcon('right-arrow'));
   }
-  if (params.location && params.location !== 'unknown') addFontAwesomeIcon(container, params.location);
-  element.prepend(container);
-}
-
-export function addFontAwesomeIcon(element: Element, text: string) {
-  let container = element.querySelector(':scope > .cyberfeeder-icon');
-  if (!container) container = element;
-  const i = document.createElement('i');
-  iconize(i, text);
-  container.appendChild(i);
-}
-
-function iconize(icon: Element, text: string) {
-  if (text === 'hq') return icon.classList.add('fa-sharp', 'fa-solid', 'fa-building', 'icon-hq');
-  if (text === 'rnd') return icon.classList.add('fa-solid', 'fa-flask', 'icon-rnd');
-  if (text === 'archives') return icon.classList.add('fa-solid', 'fa-server', 'icon-archives');
-  if (text === 'remote') return icon.classList.add('fa-sharp fa-solid', 'fa-network-wired', 'icon-remote');
-
-  if (text === 'heap') return icon.classList.add('fa-solid', 'fa-hard-drive', 'icon-stack');
-  if (text === 'stack') return icon.classList.add('fa-solid', 'fa-layer-group', 'icon-stack');
-  if (text === 'grip') return icon.classList.add('fa-solid', 'fa-hand', 'icon-grip');
-
-  if (text === 'right-arrow') return icon.classList.add('fa-solid', 'fa-arrow-right', 'icon-right');
-  if (text === 'secret') return icon.classList.add('fa-solid', 'fa-circle-question', 'icon-secret');
-  if (text === 'turn') return icon.classList.add('fa-solid', 'fa-hourglass-half', 'icon-turn');
-
-  if (text === 'access') return icon.classList.add('fa-solid', 'fa-bolt', 'icon-access');
-  if (text === 'add') return icon.classList.add('fa-solid', 'fa-circle-plus', 'icon-add');
-  if (text === 'reveal') return icon.classList.add('fa-regular', 'fa-eye', 'icon-reveal');
-  if (text === 'expose') return icon.classList.add('fa-solid', 'fa-eye', 'icon-expose');
-  return icon.classList.add('fa-solid', 'fa-question', 'icon-unknown');
+  if (annotation.location) container.appendChild(annotation.location);
+  if (annotation.secret) container.appendChild(annotation.secret);
+  return true;
 }
 
 /** annotate start of turn trigger. Returns true if it was indeed a start of turn trigger. */
-function annotateTurn(detail: chat.ChatMessage) {
-  if (detail.text.includes('started their turn')) {
-    const shouldScroll = detail.element.parentElement ? isFullyDown(detail.element.parentElement) : false;
-    const match = detail.text.match(turnRegex);
-    if (match && match.length === 2) {
-      const turn = match[1];
-      detail.element.setAttribute('turn', `Turn ${turn}`);
-      setFontAwesomeIcon(detail.element, {action: 'turn'});
-      if (shouldScroll && detail.element.parentElement) {
-        detail.element.parentElement.scrollTop = detail.element.parentElement.scrollHeight;
-      }
-    }
-    return true;
+function annotateTurn(annotation: Annotation, detail: chat.ChatMessage) {
+  if (annotation.done) return;
+  const match = detail.text.match(turnRegex);
+  if (match) {
+    annotation.done = true;
+    annotation.action = createIcon('cf-turn');
   }
-  return false;
 }
 
 /** Generic annotation based on regex search */
-function annotateGeneric(type: string, regex: RegExp | string, detail: chat.ChatMessage) {
-  const match = detail.text.match(regex);
+function annotateGeneric(annotation: Annotation, type: string, regex: MatchType[], detail: chat.ChatMessage) {
+  if (annotation.done) return;
+
+  let match: RegExpMatchArray | null = null;
+  for (const r of regex) {
+    const candidate = detail.text.match(r);
+    if (candidate) {
+      match = candidate;
+      break;
+    }
+  }
   if (!match) return;
-  const shouldScroll = detail.element.parentElement ? isFullyDown(detail.element.parentElement) : false;
   detail.element.classList.add(type);
   if (match.groups) {
     const location = util.toLocation(match.groups['location']);
     if (location !== 'unknown') {
       detail.element.setAttribute('location', location);
-      setFontAwesomeIcon(detail.element, {action: type, location});
-    }
-    if (shouldScroll && detail.element.parentElement) {
-      detail.element.parentElement.scrollTop = detail.element.parentElement.scrollHeight;
+      if (!annotation.location) annotation.location = createIcon(location);
     }
   }
+  if (!annotation.action) annotation.action = createIcon(type);
 }
