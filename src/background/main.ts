0f25033facf9564702253c940d68dc86139da487
Rhahi <git@rhahi.com>
2024-04-19 22:45:05 +0200

    feat: add alt click to inject Cyberfeeder anywhere

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,33 +1,34 @@
 /**
  * Open sidebar when clicking the extension logo on the addons list
  */
-browser.browserAction.onClicked.addListener(() => {
-  browser.sidebarAction.open();
-});
+browser.browserAction.onClicked.addListener(handleClick);
 
 /**
  * Open sidebar when clicking the extension logo on the URL bar
  * 0 = left
  * 1 = middle
  */
-browser.pageAction.onClicked.addListener(async (_, click) => {
-  if (click) {
-    if (click.button === 0) {
-      if (click.modifiers.length === 0) {
-        browser.sidebarAction.open();
-        return;
-      }
-      if (click.modifiers.includes('Ctrl')) {
-        browser.sidebarAction.close();
-        return;
-      }
-      if (click.modifiers.includes('Shift')) {
-        reloadScripts();
-        return;
-      }
-    }
+browser.pageAction.onClicked.addListener(handleClick);
+
+function handleClick(tab: browser.tabs.Tab, click?: browser.pageAction.OnClickData | browser.browserAction.OnClickData) {
+  if (!click) return;
+  if (click.button !== 0) return;
+  if (click.modifiers.length === 0) {
+    browser.sidebarAction.open();
+    return;
   }
-});
+  if (click.modifiers.includes('Ctrl')) {
+    browser.sidebarAction.close();
+    return;
+  }
+  if (click.modifiers.includes('Shift')) {
+    reloadScripts();
+    return;
+  }
+  if (click.modifiers.includes('Alt')) {
+    injectAnywhere(tab);
+  }
+}
 
 function reloadScripts() {
   browser.tabs
@@ -38,4 +39,15 @@ function reloadScripts() {
       }
     })
     .catch(() => console.warn('Could not send style to Jnet. Is jnet open?'));
+}
+
+function injectAnywhere(tab: browser.tabs.Tab) {
+  if (!tab.id) {
+    console.warn('could not find currently open tab id');
+    return;
+  }
+  browser.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['js/jnet.js'],
+  });
 }
